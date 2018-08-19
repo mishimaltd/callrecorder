efc193f8752bef412d91bb70623cf4332c1aa4cb
Pugh, Joe <joe.pugh@cengage.com>
2018-08-19 17:35:43 -0400

    Created

--- callservice/src/main/java/com/mishima/callrecorder/callservice/messaging/SQSListener.java
+++ callservice/src/main/java/com/mishima/callrecorder/callservice/messaging/SQSListener.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mishima.callrecorder.callservice.handler.EventHandler;
 import com.mishima.callrecorder.domain.entity.Event;
+import java.util.Map;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
@@ -20,13 +21,17 @@ public class SQSListener implements MessageListener {
 
   private final ObjectMapper om = new ObjectMapper();
 
+  private final TypeReference<Map<String,Object>> mapTypeReference = new TypeReference<Map<String, Object>>() {};
+  private final TypeReference<Event> eventTypeReference = new TypeReference<Event>() {};
+
   public void onMessage(Message message) {
-    log.info("Processing message {}", message);
     TextMessage textMessage = (TextMessage)message;
     try {
       String text = textMessage.getText();
       log.info("Received text {}", text);
-      Event event = om.readValue(text, new TypeReference<Event>(){});
+      Map<String,Object> map = om.readValue(text, mapTypeReference);
+      String body = (String)map.get("Message");
+      Event event = om.readValue(body, eventTypeReference);
       eventHandler.handle(event);
     } catch(Exception ex) {
       log.error("Error occurred processing message -> {}", ex);
