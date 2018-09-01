08dea66f32fc55e6ab54d6b97303327d9622ace0
Pugh, Joe <joe.pugh@cengage.com>
2018-09-01 10:01:01 -0400

    Created

---
+++ callrecorderapp/src/main/java/com/mishima/callrecorder/app/config/TwilioWebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.mishima.callrecorder.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Order(1)
+@Configuration
+public class TwilioWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private AuthenticationEntryPoint authenticationEntryPoint;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.antMatcher("/api/twilio/**")
+        .authorizeRequests().anyRequest().hasRole("TWILIO")
+        .and().httpBasic().authenticationEntryPoint(authenticationEntryPoint);
+  }
+
+}
--- callrecorderapp/src/main/java/com/mishima/callrecorder/app/config/WebSecurityConfig.java
+++ callrecorderapp/src/main/java/com/mishima/callrecorder/app/config/WebSecurityConfig.java
@@ -4,17 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 
+@Order(2)
 @Configuration
 @EnableWebSecurity
-public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Value("${twilio.username}")
   private String username;
@@ -22,24 +25,23 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
   @Value("${twilio.password}")
   private String password;
 
-  @Autowired
-  private AuthenticationEntryPoint authenticationEntryPoint;
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/", "/home", "/api/twilio/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().permitAll()
+        .and()
+        .rememberMe();
+  }
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
-        .withUser(username).password(passwordEncoder().encode(password)).authorities("TWILIO_USER");
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests()
-        .antMatchers("/api/twilio/**")
-        .authenticated()
-        .antMatchers("/**")
-        .permitAll()
-        .and().httpBasic()
-        .authenticationEntryPoint(authenticationEntryPoint);
+        .withUser(username).password(passwordEncoder().encode(password)).authorities("ROLE_TWILIO");
   }
 
   @Bean
