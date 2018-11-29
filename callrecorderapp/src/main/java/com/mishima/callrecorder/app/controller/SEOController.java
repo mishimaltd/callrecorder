81b05c986576c796b9f69d354ce68a4ef6cbaffd
Pugh, Joe <joe.pugh@cengage.com>
2018-11-28 22:16:29 -0500

    Created

--- callrecorderapp/src/main/java/com/mishima/callrecorder/app/controller/SEOController.java
+++ callrecorderapp/src/main/java/com/mishima/callrecorder/app/controller/SEOController.java
@@ -14,4 +14,29 @@ public class SEOController {
     response.getWriter().write("User-agent: *\nDisallow: /private/\nDisallow: /contact\nDisallow: /forgot\nDisallow: /login\nDisallow: /privacy\nDisallow: /register\nDisallow: /reset\nDisallow: /terms\n");
   }
 
+  @RequestMapping(value = "/sitemap.xml")
+  public void sitemap(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.getWriter().write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/</loc>\n"
+        + "  </url>\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/public/trial</loc>\n"
+        + "  </url>\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/public/services</loc>\n"
+        + "  </url>\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/public/contact</loc>\n"
+        + "  </url>\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/public/terms</loc>\n"
+        + "  </url>\n"
+        + "  <url>\n"
+        + "    <loc>http://www.mydialbuddy.com/public/privacy</loc>\n"
+        + "  </url>\n"
+        + "</urlset>\n");
+  }
+
 }
